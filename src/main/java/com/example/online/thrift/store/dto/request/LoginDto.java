9051f3ef4f217df6a32edb2ac8da6aa36907e2e1
Ritesh-Dahal <riteshdahal00@gmail.com>
2025-12-22 15:48:44 +0545

    updated

---
+++ src/main/java/com/example/online/thrift/store/dto/request/LoginDto.java
@@ -0,0 +1,15 @@
+package com.example.online.thrift.store.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LoginDto {
+    private String email;
+    private String password;
+}
